68f0755e89d25b3886be3026f14f44058a0fca69
Daniel <Daniel@130.66.205.3>
2011-09-30 10:31:41 +0200

    Corrections.

--- src/ConnexionJDBC.java
+++ src/ConnexionJDBC.java
@@ -1,52 +1,53 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 
-public class ConnexionJDBC {
+public class ConnexionJDBC
+{
+    private static final String DRIVER            = "org.postgresql.Driver";
+    private static final String EN_TETE_MYSQL     = "jdbc:postgresql://";
+    private static final String SLASH             = "/";
 
-	
-	private static final String DRIVER = "org.postgresql.Driver";
-	private static final String EN_TETE_MYSQL = "jdbc:postgresql://";
-	private static final String SLASH = "/";
-	
-	//Information pour retrouver les fichier web.xml où il y a les infos de connexion
-	
-	
-	private static String SERVEUR_BD = "dim.ec-nantes.fr";
-	private static String NOM_BD = "marchandise_11";
-	private static String UTILISATEUR_BD = "ei3info";
-	private static String MDP_BD = "ei3info";	
-	private static String CHAINE_CONNEXION = EN_TETE_MYSQL + SERVEUR_BD + SLASH + NOM_BD;	
-	
-	private Connection _connexion;
-	
-	private Boolean _estConnexionOuverte;
-		
-	private ResultSet _rs;
-	
-	
-	public ConnexionJDBC() {
-    	 _estConnexionOuverte = Boolean.FALSE;
-    	
-    	//Connexion
-    	connecter();
-	}	
+    private static String       SERVEUR_BD        = "https://vpn.vpn.ec-nantes.fr/http/0/dim.ec-nantes.fr";
+    private static String       NOM_BD            = "marchandise_11";
 
-    //Méthodes public
-    public void connecter() {	
-		if(Boolean.FALSE.equals(_estConnexionOuverte)){
-	    	try{
-				//Chargement du Driver
-		    	Class.forName(DRIVER).newInstance();
-		    
-	    	   	//Création de la connexion
-		    	_connexion = DriverManager.getConnection(CHAINE_CONNEXION, UTILISATEUR_BD, MDP_BD);
-	    	}
-	    	catch(Exception e){
-	    		System.err.println(e.getMessage());
-	    	}
-	    	
-	    	_estConnexionOuverte = Boolean.TRUE;
-		}
+    private static String       UTILISATEUR_BD    = "ei3info";
+    private static String       MDP_BD            = "ei3info";
+
+    private static String       CONNECTION_STRING = EN_TETE_MYSQL + SERVEUR_BD
+                                                          + SLASH + NOM_BD;
+
+    private Connection          connection;
+    private boolean             isConnected;
+
+    // private ResultSet resultSet;
+
+    public ConnexionJDBC()
+    {
+        // Connection
+        connecter();
+    }
+
+    public void connecter()
+    {
+        if (!this.isConnected)
+        {
+            try
+            {
+                // Loading driver
+                Class.forName(DRIVER);
+
+                // Connection creation
+                this.connection = DriverManager.getConnection(
+                        CONNECTION_STRING, UTILISATEUR_BD, MDP_BD);
+
+                this.isConnected = true;
+            } catch (Exception e)
+            {
+                System.err.println(e.getMessage());
+            }
+        }
 
     }
-          
+
 }
