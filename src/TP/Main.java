501d1863340b5259b617953637da3cb0f4e18fda
Daniel Lefevre <dan221@msn.com>
2011-09-30 18:52:37 +0200

    branch test

--- src/TP/Main.java
+++ src/TP/Main.java
@@ -3,23 +3,31 @@ package TP;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Main {
-	public static void main(String[] args) throws SQLException {
-		ConnectionJDBC myConnection = new ConnectionJDBC();
-		System.out.println("The connection is open : "
-				+ myConnection.isConnected());
+public class Main
+{
+    public static void main(String[] args) throws SQLException
+    {
+        ConnectionJDBC myConnection = new ConnectionJDBC();
+        System.out.println("The connection is open : "
+                + myConnection.isConnected());
 
-		ResultSet resultSet = myConnection
-				.query("SELECT * FROM typemarchandise");
+        Main.query(myConnection);
 
-		while (resultSet.next()) {
-			System.out.println(resultSet.getString(1) + " "
-					+ resultSet.getString(2));
-		}
+    }
 
-		myConnection
-				.queryOther("DELETE ALL");
+    public static void query(ConnectionJDBC myConnection) throws SQLException
+    {
+        ResultSet resultSet = myConnection
+                .query("SELECT * FROM typemarchandise");
 
-		myConnection.close();
-	}
+        while (resultSet.next())
+        {
+            System.out.println(resultSet.getString(1) + " "
+                    + resultSet.getString(2));
+        }
+
+        myConnection.queryOther("DELETE ALL");
+
+        myConnection.close();
+    }
 }
