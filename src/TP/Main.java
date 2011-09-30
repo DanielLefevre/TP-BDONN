811ce70d5644501c59c257431afdedfd1da52421
Daniel Lefevre <dan221@msn.com>
2011-09-30 16:54:34 +0200

    again.

---
+++ src/TP/Main.java
@@ -0,0 +1,25 @@
+package TP;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+	public static void main(String[] args) throws SQLException {
+		ConnectionJDBC myConnection = new ConnectionJDBC();
+		System.out.println("The connection is open : "
+				+ myConnection.isConnected());
+
+		ResultSet resultSet = myConnection
+				.query("SELECT * FROM typemarchandise");
+
+		while (resultSet.next()) {
+			System.out.println(resultSet.getString(1) + " "
+					+ resultSet.getString(2));
+		}
+
+		myConnection
+				.queryOther("DELETE ALL");
+
+		myConnection.close();
+	}
+}
